768e5230d724eaae5a5ae7dd714c6d3a5420c978
leagueofcake <domokun997@gmail.com>
2018-04-01 21:20:37 +1000

    Use Point implementation from eight_puzzle in sudoku solver (with row/col changed to byte type)

--- src/me/leagueofcake/sudoku/Point.java
+++ src/me/leagueofcake/sudoku/Point.java
@@ -1,12 +1,31 @@
 package me.leagueofcake.sudoku;
 
-/**
- * Point class - stores a row (y) and col (x)
- */
+import java.util.Objects;
+
 public class Point {
     public byte row, col;
+
+    /**
+     * Constructs a point from a given row and col value.
+     * @param row Row number
+     * @param col Column number
+     */
     public Point (byte row, byte col) {
         this.row = row;
         this.col = col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return row == point.row &&
+                col == point.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
     }
 }
